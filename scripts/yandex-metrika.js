e78e4c260edaaf7cccee47b786c81176ec2d1fd9
Ars Shilovsky <ars.shilovsky@gmail.com>
2013-03-26 11:40:02 +0400

    Yandex metrika test

---
+++ scripts/yandex-metrika.js
@@ -0,0 +1,32 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: stealth
+ * Date: 3/26/13
+ * Time: 11:37 AM
+ * To change this template use File | Settings | File Templates.
+ */
+<script type="text/javascript">
+(function (d, w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter17158624 = new Ya.Metrika({id:17158624,
+                webvisor:true,
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true});
+} catch(e) { }
+});
+
+var n = d.getElementsByTagName("script")[0],
+s = d.createElement("script"),
+f = function () { n.parentNode.insertBefore(s, n); };
+s.type = "text/javascript";
+s.async = true;
+s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+if (w.opera == "[object Opera]") {
+    d.addEventListener("DOMContentLoaded", f, false);
+    } else { f(); }
+})(document, window, "yandex_metrika_callbacks");
+</script>
+    <noscript><div><img src="//mc.yandex.ru/watch/17158624" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
